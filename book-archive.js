b58aa205a89e3ce42229ff2198b27a38ffaf4c03
unknown <monzurulislam997@gmail.com>
2021-09-02 23:35:57 +0600

    doneall

--- book-archive.js
+++ book-archive.js
@@ -1,33 +1,53 @@
 
-const loadBook = () => {
-    // const inputField =document.getElementById('input-field').value;
-    // console.log(inputField)
-    // const url = ` http://openlibrary.org/search.json?q=${inputField}`
-    const url = 'http://openlibrary.org/search.json?q=javaScript';
+const errorDiv =document.getElementById('errorDiv');
+const searchResultDiv=document.getElementById('resultShow-div')
+const searchNumber =document.getElementById('search-number');
+
+
+// ___loadfuntion by click___
+  const loadBook = () => {
+    searchResultDiv.innerHTML =''
+  let inputField =document.getElementById('input-field').value;
+ document.getElementById('input-field').value=' '
+    const url = ` http://openlibrary.org/search.json?q=${inputField}`
+    if(inputField ===''){ 
+      return  errorDiv.innerHTML =  `<h4> search field can not be empty  </h4>  `
+    } 
+     errorDiv.innerText =''
+     searchNumber.innerText =''
+
+     console.log(url)
+
     fetch (url)
     .then(res =>res.json())
-    .then (data=>displayBookInfo(data.docs))
+    .then (data=> displayBookInfo(data.docs)
+      )
+  
 }
 
+
+// ____displayBookInfo function_____
 const displayBookInfo = (books)=>{
-// console.log(books)
-// console.log(books.title)
-// console.log(books.author_name)
-// console.log(books.publisher)
-// console.log(books.first_publish_year)
-books.forEach(book => {
+  if(books.length=== 0){
+    return errorDiv.innerHTML = `<p> ❌No result found</p>`
+ }
+    // displaytotalbook
+const searchNumber =document.getElementById('search-number');
+ searchNumber.innerHTML = `<p>Your search number is ${books.length}</p>`
+      // _____loop for single book information___
+    books.forEach(book => { 
+    const newDiv =document.createElement('div');
+    newDiv.classList.add("col");
+    newDiv.classList.add("border");
+    newDiv.classList.add("border-2");
     
-    const searchResultDiv=document.getElementById('resultSHow-div')
-const newDiv =document.createElement('div');
-newDiv.innerHTML = ` <h2>${book.title}</h2>
-                    <h3>${book.author_name}</h3>
+    newDiv.innerHTML = ` <img width ="210px" src ="https://covers.openlibrary.org/b/id/ ${book.   cover_i}-M.jpg"></img>
+                     <h3>${book.title}</h3>
+                    <h4>${book.author_name}</h4>
                     <p>${book.publisher}</p>
                     <p>${book.first_publish_year}</p>
                             `
 searchResultDiv.appendChild(newDiv);
 });
-
-
-
 
 };
